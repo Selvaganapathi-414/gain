92f99722c37822c56d2da338d7524fd2cca3dadd
SELVAGANAPATHI-V-M <2k21cse135@kiot.ac.in>
2025-01-29 10:41:30 +0530

    changed files

--- eclipse-workspace/TechMahindraJFS/src/SampleGitHubDemo/src/com/sample/DemoSampleGit.java
+++ eclipse-workspace/TechMahindraJFS/src/SampleGitHubDemo/src/com/sample/DemoSampleGit.java
@@ -5,6 +5,8 @@ public class DemoSampleGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello ");
+		System.out.println("github");
+		System.out.println("Demo Git");
 	}
 
 }
